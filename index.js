20658d33b80a3b99d58421a562aafbd6bf9e84d5
lawrie-m <108297839+lawrie-m@users.noreply.github.com>
2022-10-21 22:10:54 +0100

    Updated versions of files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let convertBtn = document.getElementById(convert-btn)
-let inputEl = document.getElementById(input)
-let lengthEl = document.getElementById(length-el)
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
 
 const meterToFeet = 3.281
 const literToGallon = 0.264
@@ -9,6 +11,11 @@ const kiloToPound = 2.204
 convertBtn.addEventListener("click", function() {
     let baseValue = inputEl.value
 
-    lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet` 
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
+
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
+
+    massEl.textContent = `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`
+
 })
 
